f7b6bafc8be65493f240adbb1db317b0ccb99237
artisticzhao <hkcreedblacktea@hotmail.com>
2021-07-16 22:27:24 +0800

    add matrixReshape, learn 2-D array with pointer.

--- lc566_matrixReshape/test.c
+++ lc566_matrixReshape/test.c
@@ -2,42 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 
-int** matrixReshape(int** mat, int matSize, int* matColSize, int r, int c, int* returnSize, int** returnColumnSizes) {
-    // cannot reshape
-    if (matSize * (*matColSize) != r * c) {
+int** matrixReshape(int** mat, int matSize, int* matColSize, int r, int c, int* returnSize, int** returnColumnSizes){
+    if(matSize*matColSize[0] != r*c) {
         *returnSize = matSize;
-        *returnColumnSizes = (int*)malloc(sizeof(int) * (*returnSize));
-        memcpy((*returnColumnSizes), matColSize, sizeof(int) * matSize);
+        *returnColumnSizes = matColSize;
         return mat;
     }
-
-    // reshape
+    // for(int i=0; i<matSize; ++i){
+    //     for(int j=0; j<matColSize[i]; ++j){
+    //         // printf("%d, %d: %d \n", i,j,*(*(mat+i)+j));
+    //         printf("%d ", mat[i][j]);
+    //     }
+    // }
+    int** res = (int**)malloc(sizeof(int*)*r);
     *returnSize = r;
-    *returnColumnSizes = (int*)malloc(sizeof(int) * (*returnSize));
-    memset(*returnColumnSizes, c, r);
-    for (int i = 0; i < matSize; ++i) {
-        for (int j = 0; j < matColSize[i]; ++j)
-            printf("%d ", *(*(mat) + j)); //*matColSize[i] +j
-        printf("\n");
+    *returnColumnSizes = (int*)malloc(sizeof(int)*r);
+    for(int i=0; i<r; ++i){
+        res[i] = (int*)malloc(sizeof(int)*c);
+        *(*returnColumnSizes+i)= c;
+        for(int j=0; j<c; ++j){
+            int t = i*c+j;
+            res[i][j] = mat[t/matColSize[0]][t%matColSize[0]];
+        }
     }
-    return mat;
+    printf("%d*%d",*returnSize, *(*returnColumnSizes));
+    return res;
 }
 
 int main() {
     int arr[][2] = { {1,2}, {3,4} };
     int matSize = 2;
     int matColSize[] = { 2,2 };
+    int** arr_in = (int**)malloc(sizeof(int*)*2);
     for (int i = 0; i < matSize; ++i) {
-        for (int j = 0; j < matColSize[i]; ++j)
-            printf("%d ", *(*(arr) +i*2+ j));
-        printf("\n");
+        arr_in[i] = (int*)malloc(sizeof(int)*matColSize[i]);
+        for(int j=0; j<matColSize[i]; ++j){
+            arr_in[i][j] = arr[i][j];
+        }
     }
+
     int returnSize;
     int* returnColumnSizes;
-    int** arr2 = matrixReshape((int **)&(arr), 2, matColSize, 1, 4, &returnSize, &returnColumnSizes);
+    int** arr2 = matrixReshape(arr_in, 2, matColSize, 1, 4, &returnSize, &returnColumnSizes);
     printf("----\n");
     for (int i = 0; i < returnSize; ++i) {
-        for (int j = 0; j < matColSize[i]; ++j)
+        for (int j = 0; j < returnColumnSizes[i]; ++j)
             printf("%d ", *(*(arr2+i)+j));
         printf("\n");
     }
